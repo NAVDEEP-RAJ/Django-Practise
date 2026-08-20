28dfea9b2dd40a5181db0a826ddfe287ded9341a
Navdeep Raj <rajnavdeep8@gmail.com>
2026-08-21 00:15:27 +0530

    Looped arguments in the home template

--- Django-Practise/studybuddy/studbud/base/views.py
+++ Django-Practise/studybuddy/studbud/base/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+rooms=[
+    {'id': 1,'name':"Let's learn python"},
+    {'id':2,'name':"It is easy language"},
+    {'id':3,'name':'Third page'}
+
+]
+
 def home(request):
-    return render(request,'home.html')
+    return render(request,'home.html',{'rooms':rooms})
 
 def room(request):
     return render(request,'rooms.html')
