f413728fc361850799b8e592de2c662083ffc076
Navdeep Raj <rajnavdeep8@gmail.com>
2026-08-22 23:28:10 +0530

    New models have been migrated and added to the admin.py

--- Django-Practise/studybuddy/studbud/base/admin.py
+++ Django-Practise/studybuddy/studbud/base/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 
-from .models import Rooms
+from .models import Rooms,topic,Message
 
 admin.site.register(Rooms)
+admin.site.register(Topic)
+admin.site.register(Message)
 # Register your models here.
